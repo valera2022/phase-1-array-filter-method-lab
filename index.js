409a34ff03dd65033f4ef1643a5b535cf6b30197
GeralmyUrena <geralmyuri11@gmail.com>
2022-11-29 17:55:08 -0500

    commit tree 2245e9b1e40ad00b71f056ef0cbc7c2c107e9946

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(d,string){
+    return d.filter(x => x.toUpperCase() === string.toUpperCase()) 
+   
+}
+
+function fuzzyMatch(d, string){
+    return d.filter(x => x.indexOf(string) === 0) // bobby.indexOf(bob) index is 0(starting index)
+// //let Nd = d.filter(function(x){
+//     if (x === string){return Nd}
+//     else if (string === string){
+//         return x[0]
+//     }
+//     return Nd 
+//  })
+    
+}
+
+function matchName(d,string){
+    return d.filter(a => a.name === string )
+
+}
+
+
